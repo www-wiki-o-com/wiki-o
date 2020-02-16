b9b55f73786e13ddb8230eb62697979ee27020cd
Frank Imeson <fcimeson@gmail.com>
2020-02-15 20:32:05 -0500

    Issue #18: Moved www.wiki-o.com/admin to admin.wiki-o.com.

---
+++ wiki_o/admin_urls.py
@@ -0,0 +1,32 @@
+"""  __      __    __               ___
+    /  \    /  \__|  | _ __        /   \
+    \   \/\/   /  |  |/ /  |  __  |  |  |
+     \        /|  |    <|  | |__| |  |  |
+      \__/\__/ |__|__|__\__|       \___/
+
+A web service for sharing opinions and avoiding arguments
+
+@file       wiki_o/admin_urls.py
+@brief      A collection of app specific, admin urls
+@details    The `urlpatterns` list routes URLs to views. For more information please see:
+                https://docs.djangoproject.com/en/2.1/topics/http/urls/
+            Examples:
+            Function views
+                1. Add an import:  from my_app import views
+                2. Add a URL to urlpatterns:  path('', views.home, name='home')
+            Class-based views
+                1. Add an import:  from other_app.views import Home
+                2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+            Including another URLconf
+                1. Import the include() function: from django.urls import include, path
+                2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+@copyright  GNU Public License, 2018
+@authors    Frank Imeson
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('', admin.site.urls),
+]
