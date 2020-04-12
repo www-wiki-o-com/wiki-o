406323f20726ee5abf9e025e24e5929fe2c1f50c
Frank Imeson <fcimeson@gmail.com>
2020-04-12 07:21:51 -0400

    Issue #152: Updated export scripts.

--- wiki_o/urls.py
+++ wiki_o/urls.py
@@ -4,24 +4,24 @@
      \        /|  |    <|  | |__| |  |  |
       \__/\__/ |__|__|__\__|       \___/
 
-A web service for sharing opinions and avoiding arguments
+Copyright (C) 2018 Wiki-O, Frank Imeson
 
-@file       wiki_o/urls.py
-@brief      A collection of app specific, front end urls
-@details    The `urlpatterns` list routes URLs to views. For more information please see:
-                https://docs.djangoproject.com/en/2.1/topics/http/urls/
-            Examples:
-            Function views
-                1. Add an import:  from my_app import views
-                2. Add a URL to urlpatterns:  path('', views.home, name='home')
-            Class-based views
-                1. Add an import:  from other_app.views import Home
-                2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-            Including another URLconf
-                1. Import the include() function: from django.urls import include, path
-                2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-@copyright  GNU Public License, 2018
-@authors    Frank Imeson
+This source code is licensed under the GPL license found in the
+LICENSE.md file in the root directory of this source tree.
+
+This file contains a collection of app specific, admin urls. The `urlpatterns` list routes URLs
+to views. For more information please see: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+Examples:
+    Function views
+        1. Add an import:  from my_app import views
+        2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    Class-based views
+        1. Add an import:  from other_app.views import Home
+        2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    Including another URLconf
+        1. Import the include() function: from django.urls import include, path
+        2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
 # *******************************************************************************
